a2a7a4d796d8293d574ff7cfba4b8d70e38d76a2
Magda Krawczyk <magdalena.a.krawczyk@gmail.com>
2017-10-01 13:00:37 +0200

    zadanie 9-5

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var buttonArray = document.getElementsByClassName('button');
+
+console.log(buttonArray);
+
+for (var i = 0; i < buttonArray.length; i++) {
+  console.log(buttonArray[i].innerText);
+}
